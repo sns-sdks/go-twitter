7aa27582fe0cc9002a5ea150b13fa4c007059505
ikaroskun <merle.liukun@gmail.com>
2021-09-29 02:58:13 +0800

    test(tests): :white_check_mark: update tests for get muting

--- twitter/user_mutes_test.go
+++ twitter/user_mutes_test.go
@@ -2,6 +2,33 @@ package twitter
 
 import "github.com/jarcoal/httpmock"
 
+func (uc *UCSuite) TestGetMuting() {
+	uid := "1324848235714736129"
+
+	httpmock.RegisterResponder(
+		HttpGet, Baseurl+"/users/"+uid+"/muting",
+		httpmock.NewStringResponder(
+			401,
+			`{"title":"Unauthorized","type":"about:blank","status":401,"detail":"Unauthorized"}`,
+		),
+	)
+	_, err := uc.Tw.Users.GetMuting(uid, MutingOpts{})
+	uc.IsType(&APIError{}, err)
+
+	httpmock.RegisterResponder(
+		HttpGet, Baseurl+"/users/"+uid+"/muting",
+		httpmock.NewStringResponder(
+			200,
+			`{"data":[{"id":"2244994945","name":"Twitter Dev","username":"TwitterDev"}],"meta":{"result_count":1}}`,
+		),
+	)
+
+	resp, _ := uc.Tw.Users.GetMuting(uid, MutingOpts{})
+	uc.Equal(*resp.Meta.ResultCount, 1)
+	uc.Equal(*resp.Data[0].ID, "2244994945")
+
+}
+
 func (uc *UCSuite) TestCreateMuting() {
 	uid := "123456789"
 	targetID := "2244994945"
