e755eb63c8cbf9dfc527fe2db369d4c38306916f
ikaroskun <merle.liukun@gmail.com>
2022-04-07 17:01:24 +0800

    test(tests): :white_check_mark: update tests

--- twitter/authorization_oauth2_test.go
+++ twitter/authorization_oauth2_test.go
@@ -15,9 +15,10 @@ type Auth2Suite struct {
 
 func (auth *Auth2Suite) SetupSuite() {
 	auth.app = NewOAuth2AuthorizationAPP(OAuth2AuthorizationAPP{
-		ClientID:    "client id",
-		CallbackURL: "https://localhost/",
-		Scopes:      []string{"users.read", "tweet.read"},
+		ClientID:     "client id",
+		ClientSecret: "client secret",
+		CallbackURL:  "https://localhost/",
+		Scopes:       []string{"users.read", "tweet.read"},
 	})
 }
 
